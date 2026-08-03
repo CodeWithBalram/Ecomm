29ff5198fa478be4ec6eb855309c20564b34560d
CodeWithBalram <balramchaurasiya06@gmail.com>
2026-08-03 17:50:35 +0530

    Bulid api rewrite features

---
+++ apigateway/src/main/java/com/ecom/apigateway/controller/FallbackController.java
@@ -0,0 +1,15 @@
+package com.ecom.apigateway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController
+{
+    @GetMapping("/fallback/product")
+    public ResponseEntity<String> fallbackProduct()
+    {
+        return ResponseEntity.ok("Product service is currentlt unavailable");
+    }
+}
