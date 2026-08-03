d6887412d95864b8bb0b480042f42b6e484609e5
CodeWithBalram <balramchaurasiya06@gmail.com>
2026-08-03 13:20:28 +0530

    implements redis features of rate limit

---
+++ apigateway/src/main/java/com/ecom/apigateway/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.ecom.apigateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RedisConfig
+{
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest()
+                        .getRemoteAddress()
+                        .getAddress()
+                        .getHostAddress()
+        );
+
+    }
+
+}
