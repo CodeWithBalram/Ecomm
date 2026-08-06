0428dc81f21c12563b0d747c3858f778b4cd8344
CodeWithBalram <balramchaurasiya06@gmail.com>
2026-08-06 12:26:28 +0530

    implements discovery service

--- order_service/src/main/java/com/ecom/order_service/config/RestTemplateConfig.java
+++ order_service/src/main/java/com/ecom/order_service/config/RestTemplateConfig.java
@@ -2,6 +2,8 @@ package com.ecom.order_service.config;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -11,13 +13,10 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
+    @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder)
+    {
+        return builder.build();
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory(httpClient);
-
-        return new RestTemplate(factory);
     }
 }
